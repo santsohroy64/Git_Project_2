4823e77c406a11dc4244936a30ae9e8f2db0a940
Santosh Roy <santoshroy64@gmail.com>
2018-12-29 13:26:35 +0530

    My secong Git

---
+++ hello.py
@@ -0,0 +1,40 @@
+# 1.
+# print("Hello World")
+# print("this is \\\\ double backslash")
+# print("these are /\\/\\/\\/\\ mountains")
+# print("he is \t awesome") # do not use escape sequence
+# print(" \\\" \\n \\t \\   a\"  ")
+# print(r"Hello \n World")
+
+# 2. Emoji
+# print("\U0001F604")
+# print("\U0001F602")
+
+# 3. Calculator
+# print(2+3*4)
+# print(4/2) # float div
+# print(4//2)# int div
+# print(2/4)
+# print(2**3)# exponent
+# print(2**0.5)# exponent
+# print(round(2**0.5,4))# exponent
+# print(2**3/2*6-4*(3-4/2))# exponent
+# print(2**3**2)# exponent
+
+# 4. Variables
+# num1 = 2
+# print(num1)
+# name = "santosh"
+# print(name)
+# name = 123
+# print(name)
+
+# Rules for variables:
+# 1number = 4, special chacr #,num&ber -> not allowed
+# letter,_,num123ber -> allowed
+
+Convention or Recommendatio
+Num_123_ber: snake case writting
+Num_123_Ber: camel case writting
+
+
